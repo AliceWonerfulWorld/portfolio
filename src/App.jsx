afb4129654e228ba096ad2b6d20d43f423c63c48
AliceWonerfulWorld <gomaazarasi1789@gmail.com>
2025-06-02 13:05:25 +0900

    [update] ソーシャルリンクの修正とスタイル調整

--- src/App.jsx
+++ src/App.jsx
@@ -92,9 +92,6 @@ function App() {
               <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer">
                 <i className="fab fa-github"></i>
               </a>
-              <a href="https://linkedin.com/in/yourusername" target="_blank" rel="noopener noreferrer">
-                <i className="fab fa-linkedin"></i>
-              </a>
               <a href="https://twitter.com/yourusername" target="_blank" rel="noopener noreferrer">
                 <i className="fab fa-twitter"></i>
               </a>
