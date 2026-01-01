1a46247629087803a66ea5460ace4b1ed6fa2b93
AliceWonerfulWorld <gomaazarasi1789@gmail.com>
2026-01-02 00:50:15 +0900

    feat: プロジェクトの追加・編集機能を備えた`ProjectEditor`コンポーネントを新規作成

--- src/components/admin/ProjectEditor.jsx
+++ src/components/admin/ProjectEditor.jsx
@@ -24,5 +24,30 @@ const ProjectEditor = () => {
         detailsLink: '',
     });
 
-    
+    // 追加モード開始
+    const handleAdd = () => {
+        setEditingProject(createEmptyProject());
+        setIsAdding(true);
+    };
+
+    // 編集モード開始
+    const handleEdit = (project, index) => {
+        setEditingProject({ ...project, index });
+        setIsAdding(false);
+    };
+
+    // 保存 (新規 or 更新)
+    const handleSave = () => {
+        if (isAdding) {
+            // 新規追加
+            setProjects([...projects, editingProject]); 
+        } else {
+            // 更新
+            const newProjects = [...projects];
+            newProjects[editingProject.index] = editingProject;
+            setProjects(newProjects)
+        }
+        setEditingProject(null);
+        setIsAdding(false);
+    };
 }
